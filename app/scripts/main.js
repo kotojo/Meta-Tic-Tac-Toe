2c364b21c6d696e247896878220ec1343815f78c
Bruce <brucek17@hotmail.com>
2015-05-14 14:45:35 -0400

    Now has animation for winner, and visible text for who is winner. Now can't play in completed game board. Added restart button.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
   'use strict';
 
   var turn = 'X'; //initalizes turn
+  var gameClone;
+  var copy = function() {
+    gameClone = $('.game').clone();
+  };
+
+  copy(); //makes a duplicate of the game to restart with
+
+  $('#restart').hide();
 
   var playerTurn = function () {
     $('#playerTurn').text(turn + '\'s turn');
@@ -11,35 +19,48 @@ $(document).ready(function() {
   var compActive = function() {
     var square = Math.floor((Math.random() * 9) + 1);
     var choice = '#outer'+square.toString();
-    $(choice).addClass('active');
+    if ($(choice).hasClass('x') || $(choice).hasClass('o')) {
+      compActive();
+    }
+    else {
+      $(choice).addClass('active');
+    }
   };
 
   compActive();
 
   playerTurn();
 
-    $('div > div.innerbox').click(function(){                //click function to play
+    $('#restart').click(function() {
+      $('.game').replaceWith(gameClone);
+      compActive();
+      $('#restart').hide();
+    });
+
+    $('div').on('click', 'div.innerbox', function(event){                //click function to play
+      event.stopPropagation();
       var that = $(this);
-      if ($(this).parents('.outerbox').hasClass('active')){
+      if ($(this).parents('.outerbox').hasClass('active')){ //if clickedsquare is active do this
         if(that.text() === 'X' || that.text() === 'O'){
 
           that.addClass('animated shake'); //shakes if already full
           setTimeout(function() {
             that.removeClass('animated shake');
           }, 500);
+          return;
         }
         else {
           that.text(turn);
         }
 
-        getInsideWinner(that);
+        getInsideWinner(that); //checks winner for both inside and outside
         getOutsideWinner();
 
         that.parents('.outerbox').removeClass('active');
 
         if ($('input[type="checkbox"]').is(':checked')) {
           compActive();
-          if (turn === 'X') { //changes player
+          if (turn === 'X') { //changes player if computer is changing active
             turn = 'O';
           }
           else {
@@ -47,25 +68,35 @@ $(document).ready(function() {
           }
         }
       }
-      else if ($('.outerbox').hasClass('active')) {
+      else if ($('.outerbox').hasClass('active')) { //checks if click on square that isn't active, but one of the others is.
         return;
       }
 
-      else {
+      else {                                                  //add class click, also has logic to have comp choose if checkbox is checked
         if ($('input[type="checkbox"]').is(':not(:checked)')) {
-          that.parents('.outerbox').addClass('active');
+          if (that.parents('.outerbox').hasClass('x') || that.parents('.outerbox').hasClass('o')) {
+
+            that.parents('.outerbox').addClass('animated shake'); //shakes if already won
+            setTimeout(function() {
+              that.parents('.outerbox').removeClass('animated shake');
+            }, 500);
+            return;
+          }
+          else {
+            that.parents('.outerbox').addClass('active');
+          }
         }
         else {
           compActive();
         }
-        if (turn === 'X') { //changes player
+        if (turn === 'X') { //changes player if they are choosing active square
         turn = 'O';
         }
         else {
           turn = 'X';
         }
       }
-      playerTurn();
+      playerTurn();  //updates who's turn it is
     });
 
    var xWon = function(z) {
@@ -78,35 +109,38 @@ $(document).ready(function() {
      w.parents('.outerbox').addClass('o');
    };
 
+   var outerWin = function (winner) {
+    $('#outer5').parent().siblings().toggle('explode', {pieces: 128}, 2000);
+    $('#outer5').parent().html(winner.toUpperCase() + ' is winner!');
+    $('#restart').show();
+   };
+
    var getOutsideWinner = function () { //when someone wins inside game it adds class x or o, this checks those for full winner.
     var y = 'x';
     for (var i=0; i<2; i++) {
       if ($('#outer1').hasClass(y) && $('#outer2').hasClass(y) && $('#outer3').hasClass(y)) {
-        console.log(y + 'is winner!');
+        outerWin(y);
       }
       else if ($('#outer4').hasClass(y) && $('#outer5').hasClass(y) && $('#outer6').hasClass(y)) {
-        console.log(y + 'is winner!');
+        outerWin(y);
       }
       else if ($('#outer7').hasClass(y) && $('#outer8').hasClass(y) && $('#outer9').hasClass(y)) {
-        console.log(y + 'is winner!');
+        outerWin(y);
       }
       else if ($('#outer1').hasClass(y) && $('#outer4').hasClass(y) && $('#outer7').hasClass(y)) {
-        console.log(y + 'is winner!');
+        outerWin(y);
       }
       else if ($('#outer2').hasClass(y) && $('#outer5').hasClass(y) && $('#outer8').hasClass(y)) {
-        console.log(y + 'is winner!');
+        outerWin(y);
       }
       else if ($('#outer3').hasClass(y) && $('#outer6').hasClass(y) && $('#outer9').hasClass(y)) {
-        console.log(y + 'is winner!');
+        outerWin(y);
       }
       else if ($('#outer1').hasClass(y) && $('#outer5').hasClass(y) && $('#outer9').hasClass(y)) {
-        console.log(y + 'is winner!');
+        outerWin(y);
       }
       else if ($('#outer3').hasClass(y) && $('#outer5').hasClass(y) && $('#outer7').hasClass(y)) {
-        console.log(y + 'is winner!');
-      }
-      else{
-        return;
+        outerWin(y);
       }
       y = 'o';
     }
